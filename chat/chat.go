82f41585c71162763d40fd16d21ac6f5942c2e1d
snowdreamofwind <34260052+snowdreamofwind@users.noreply.github.com>
2018-04-09 11:13:55 +0800

    Update chat.go

--- chat/chat.go
+++ chat/chat.go
@@ -1,1 +1,91 @@
+// 建立一个聊天服务器使得客户端可以互相聊天
 
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+//!+broadcaster
+type client chan<- string // 对外发送消息的通道
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // 所有用户的消息
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // 所有连接到服务器的客户端
+	for {
+		select {
+		case msg := <-messages:
+			// 将接收到的消息广播给所有客户端
+			// 发送消息的通道
+			for cli := range clients {
+				cli <- msg
+			}
+
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+//!-broadcaster
+
+//!+handleConn
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // 对外发送客户端消息的通道
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	entering <- ch
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+	// 忽略input.Err()中可能的错误
+
+	leaving <- ch
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // 忽略网络的错误
+	}
+}
+
+//!-handleConn
+
+//!+main
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8001") //监听8001端口的tcp连接
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+//!-main
